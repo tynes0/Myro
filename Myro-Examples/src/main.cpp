07ebcfba8f984265a37082a71c3ae062cf23ae75
tynes0 <cihanbilgihan@gmail.com>
2026-02-19 14:46:28 +0300

    Fixed a bug that prevented Speex assemblies from running in certain situations. The default startup project has been changed to Myro-Examples.

--- Myro-Examples/src/main.cpp
+++ Myro-Examples/src/main.cpp
@@ -13,10 +13,9 @@ int main()
 	std::vector<std::filesystem::path> files{ "assets/mp3_test.mp3", "assets/wav_test.wav", "assets/ogg_vorbis_test.ogg", "assets/opus_test.opus", "assets/flac_test.flac", "assets/ogg_flac_test.ogg" };
 
 	coco::timer<coco::time_units::milliseconds> timer;
-
 	std::vector<std::shared_ptr<myro::audio_source>> tests = myro::audio_engine::multi_load_audio_source(files);
-
 	timer.stop();
+
 	myro::log::error("Total loading took: {0}ms", timer.get_time());
 
 	myro::audio_engine::shutdown();
